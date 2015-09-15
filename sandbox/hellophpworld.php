f5d64296c0d570b4af778bba27e55bea17dce7b6
joyzoso <joyrockon@gmail.com>
2015-09-14 23:25:09 -0700

    Random PHP
    More random exercises from school

---
+++ sandbox/hellophpworld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello PHP World</title>
+	</head>
+<body>
+	<?php 
+	//writing comments now
+	?>
+		<?php echo "Hello PHP World!";?><br/>
+		<?php echo "Hello" . " PHP World!"; ?><br/>
+		<?php echo 2 + 3;?>
+		
+		
+<h1>My First Heading</h1>
+
+<p>My first paragraph</p>
+
+</body>
+</html>
